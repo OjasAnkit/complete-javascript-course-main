5e3acd5a25ba5b21df13ebd238ac3ad49cdad40b
Ojas Ankit <ojasankit@gmail.com>
2021-08-28 16:23:14 +0530

    Updated coding challenge part 2

--- Fundamentals Part-2/Coding challenge part 2/script.js
+++ Fundamentals Part-2/Coding challenge part 2/script.js
@@ -1,4 +1,8 @@
-bills = [125, 555, 44];
+const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+let tips = [];
+let totalAmount = [];
+
+let avg;
 
 function calcTip(bill) {
     if (bill >= 50 && bill <= 300)
@@ -6,10 +10,22 @@ function calcTip(bill) {
     else
         return bill * 0.2;
 }
+
+function calcAverage(arr) {
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++)
+        sum += arr[i];
+    return sum / arr.length;
+}
+
+for (let num = 0; num < bills.length; num++) {
+    tips.push(calcTip(bills[num]));
+    totalAmount.push(tips[num] + bills[num]);
+}
+
+avg = calcAverage(totalAmount);
+
 console.log(`The bills are ${bills}`);
-
-tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
 console.log(`The tips are ${tips}`);
-
-totalAmount = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
-console.log(`The total amount is ${totalAmount}`);
+console.log(`The total amount(bill + tip) is ${totalAmount}`);
+console.log(`The avg of total amount is ${avg}`);
